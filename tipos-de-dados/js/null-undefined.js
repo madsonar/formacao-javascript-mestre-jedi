6ec77848302404822336e3fd295add784dba2253
= <=>
2018-04-08 15:15:39 -0300

    Tipos de dados

---
+++ tipos-de-dados/js/null-undefined.js
@@ -0,0 +1,22 @@
+var nome;
+console.log(nome);
+nome = "Prof. Madson";
+console.log(nome);
+
+nome = undefined;//já mais usar
+nome = null;//liberar espaço em memória
+
+console.log("undefined == null", undefined == null);
+console.log("undefined", typeof undefined);
+console.log("null", typeof null);
+console.log("undefined === null", undefined === null);
+
+//var idade;
+//idade.nome;
+var pessoa = {nome: 'pedro'};
+console.log(pessoa.idade)
+console.log(pessoa.idade.value)
+
+
+
+
