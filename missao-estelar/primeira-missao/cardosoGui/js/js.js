372ad613f17486d79731ade5fd466f499a4fd05a
Guilherme Cardoso de Almeida <igui.trb20@outlook.com>
2018-05-13 11:54:04 -0300

    CardosoGui iniciando a missão estelar Mestre Jedi JS

---
+++ missao-estelar/primeira-missao/cardosoGui/js/js.js
@@ -0,0 +1,10 @@
+var missao = "Missao estelar em execução";
+
+function eventoOnload() {
+  console.log("Evento disparado com sucesso!");
+  console.log(missao);
+}
+
+function eventoOnclick() {
+  console.log("Missão estelar realizada com sucesso!");
+}
