96760a08329fa2cd0a0c89ffd93a493d8feb4f31
= <=>
2018-05-14 15:53:21 -0300

    Aulas Operadores

--- tipos-de-dados/js/regexp.js
+++ tipos-de-dados/js/regexp.js
@@ -6,3 +6,13 @@ var stringCurso = "Formação Completa JavaScript - Mestre JS Jedi";
 console.log(regexpLiteral.test(stringCurso));
 console.log(regexpConstrutor.exec(stringCurso));
 
+//recuperar todoas as ocorrências de js no texto
+var expTodas = new RegExp("j","ig");
+var resultado = stringCurso.match(expTodas);
+console.log("Tipo resultado: ", Object.prototype.toString.call(resultado));
+console.log("Qtde elementos: ", resultado.length);
+console.log(resultado);
+for (let index = 0; index < resultado.length; index++) {
+    var element = resultado[index];
+    console.log("Elemento: "+ element);
+}
