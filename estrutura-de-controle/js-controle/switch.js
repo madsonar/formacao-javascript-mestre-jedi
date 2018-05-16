5b5bc9c7c523f2d24bd302c5dccc659c57b70c78
= <=>
2018-05-16 11:24:40 -0300

    Aulas estrutura de controle Loop

---
+++ estrutura-de-controle/js-controle/switch.js
@@ -0,0 +1,19 @@
+var curso = "JS";
+
+switch (curso) {
+    case "js":
+        console.log("O valor é js!");
+        break;
+
+    case "Js":
+        console.log("O valor é Js!");
+        break;
+
+    case "JS":
+        console.log("O valor é JS!");
+        break;
+
+    default:
+        console.log("Não encontrou a comparação correta.");
+        break;
+}
