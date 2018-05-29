1c4a71aeec2706f38f967d88671f12d8f0bc9f7d
Admilson <admilsonsms@hotmail.com>
2018-05-29 15:21:17 -0300

    Missão Estelar JS - Realizada com sucesso!!! :)

---
+++ missao-estelar/primeira-missao/admilsonsms/js/js.js
@@ -0,0 +1,11 @@
+//Declarações de variáveis
+var missao = "Missão Estelar JS em execução!!!";
+
+//Declarações de funções
+function eventoOnload(){
+    console.log("Evento onload disparado");
+}
+
+function eventoOnclick (){
+    console.log("Missão Estelar JS realizada com sucesso!!!");
+}
