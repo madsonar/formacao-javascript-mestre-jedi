80f02baf24ef5b87d30c3b2966918325a865fece
HernaniIke <hnij10@gmail.com>
2018-06-03 07:50:51 -0300

    1ª Missão concluida(Hernaniike)

---
+++ missao-estelar/primeira-missao/hernaniike/js/js.js
@@ -0,0 +1,9 @@
+var missao = 'Missão Estelar JS em execução!!!'
+
+function eventoOnLoad(){
+    console.log("Evento OnLoad Disparado!");
+}
+
+function eventoOnClick(){
+    console.log("Missão estelar JS realizada com sucesso!!!")
+}
