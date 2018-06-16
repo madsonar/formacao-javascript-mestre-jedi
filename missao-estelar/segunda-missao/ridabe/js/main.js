64a099ef9a515b8a3838d3370c2925089e58ffed
ridabe <ridabe@uol.com.br>
2018-06-16 16:55:35 -0300

    segunda missao

--- missao-estelar/segunda-missao/ridabe/js/main.js
+++ missao-estelar/segunda-missao/ridabe/js/main.js
@@ -19,6 +19,7 @@ var nomeCliente = 'Ricardo Bene';
 var renda = 5000;
 var dataNascimento = new Date('1978-08-27');
 var ativo = true;
+
 function exibirDadosClienteVariaveis() {
     console.log("Nome: " + nomeCliente);
     console.log("Renda: " + renda);
@@ -30,11 +31,19 @@ exibirDadosClienteVariaveis();
 
 //11
 console.log("------------Exercico 11");
-var clienteArray = [
-    { nomeCliente: 'Ricardo Bene' },
-    { renda: 5000 },
-    { dataNascimento: new Date('1978-08-27') },
-    { ativo: true }];
+var clienteArray = [{
+        nomeCliente: 'Ricardo Bene'
+    },
+    {
+        renda: 5000
+    },
+    {
+        dataNascimento: new Date('1978-08-27')
+    },
+    {
+        ativo: true
+    }
+];
 
 function exibirDadosClienteArray() {
     for (var i of clienteArray) {
@@ -52,26 +61,27 @@ var clienteObject = {
     ativo: true
 };
 
-function exibirDadosClienteObjeto(){
+function exibirDadosClienteObjeto() {
     for (var key in clienteObject) {
-       console.log(key,": ",clienteObject[key]);
+        console.log(key, ": ", clienteObject[key]);
     }
 };
 exibirDadosClienteObjeto();
 
 //13
 console.log("------------Exercico 13");
-function retornaDataAtualFormatada(){
+
+function retornaDataAtualFormatada() {
     var dataString = new Date();
     var ano = dataString.getFullYear();
-    var mes = dataString.getMonth()+1;
-    if(mes.toString.length == 1)
-        mes="0"+mes+'/';
-       
-    
+    var mes = dataString.getMonth() + 1;
+    if (mes.toString.length == 1)
+        mes = "0" + mes + '/';
+
+
     var dia = dataString.getDay();
-    if(dia.toString.length == 1)
-    dia = "0"+dia+'/';
+    if (dia.toString.length == 1)
+        dia = "0" + dia + '/';
 
     var novaData = dia;
     novaData += mes;
@@ -79,3 +89,102 @@ function retornaDataAtualFormatada(){
     console.log(novaData);
 }
 retornaDataAtualFormatada();
+
+//14
+console.log("------------Exercico 14");
+var hiperEspacoRegExp = new RegExp("Estelar");
+var textoMissao = "Apertem os cintos para adentramos ao hiper-espaço rumo a uma nova Missão Estelar JS!:)";
+console.log(hiperEspacoRegExp.test(textoMissao)); //Teste para verificar se existe a expressao regular setada dentro da variavel string "Retorna um boolean"
+console.log(hiperEspacoRegExp.exec(textoMissao));
+
+//15
+console.log("------------Exercico 15");
+try {
+    for (var i = 30; i >= 0; i--) {
+
+        console.log(i);
+
+        if (i == 1) { //Forçar um erro usando o trow para ir para o bloco catch
+            throw "Forcei um erro na iteracao 29";
+        }
+    }
+
+} catch (error) {
+    console.log("Detalehe do Erro!", error);
+
+} finally {
+    console.log("Obrigado!");
+}
+
+//16
+console.log("------------Exercico 16");
+
+function recebeDadosFormBoot(elementos) {
+    var objFormElementos = document.querySelector(elementos);
+    console.log("Objeto tipo: ", Object.prototype.toString.call(objFormElementos));
+    var objDadosForm = new Object;
+    objDadosForm.nome = objFormElementos[0].value;
+    objDadosForm.email = objFormElementos[1].value;
+    recebeEmail = objFormElementos[2].checked;
+
+    //var recebeEmail = document.getElementById("emailPromocionalCheckBoot").checked;
+
+    if (recebeEmail == true) {
+        objDadosForm.emailPromocionalCheckBoot = "Sim";
+    } else {
+        objDadosForm.emailPromocionalCheckBoot = "Não";
+    }
+
+
+    var formaContatoTelefoneRadio = document.querySelector("[id=formaContatoTelefoneRadioBoot]");
+    if (formaContatoTelefoneRadio.checked == true) {
+        objDadosForm.formaContatoRadioBoot = objFormElementos[3].value;
+
+    } else {
+        objDadosForm.formaContatoRadioBoot = objFormElementos[4].value;
+    }
+
+    objDadosForm.estadoSelectBoot = objFormElementos[5].value;
+
+
+    console.log(objDadosForm.nome);
+    console.log(objDadosForm.email);
+    console.log(objDadosForm.emailPromocionalCheckBoot);
+    console.log(objDadosForm.formaContatoRadioBoot);
+    console.log(objDadosForm.estadoSelectBoot);
+
+    console.log("propriedades do objeto: ", objDadosForm.toString());
+
+
+    preencherFormHtml(objDadosForm); //chama a funcao que preenche os campos html
+};
+
+
+
+
+function preencherFormHtml(objDados) {
+    document.getElementById("nomeHtml").value = objDados.nome;
+    document.getElementById("emailHtml").value = objDados.email;
+
+    if (objDados.emailPromocionalCheckBoot == "Sim") {
+        document.getElementById("emailPromocionalCheckHtml").checked = true;
+    } else {
+        document.getElementById("emailPromocionalCheckHtml").checked = false;
+    }
+
+    if (objDados.formaContatoRadioBoot == "Telefone") {
+
+        document.getElementById("formaContatoTelefoneRadioHtml").checked = true;
+        document.getElementById("formaContatoEmailRadioHtml").checked = false;
+
+    } else {
+
+        document.getElementById("formaContatoTelefoneRadioHtml").checked = false;
+        document.getElementById("formaContatoEmailRadioHtml").checked = true;
+
+    }
+
+    document.getElementById("estadoSelectHtml").value = objDados.estadoSelectBoot
+
+    //console.log(nomeHtml);
+};
