ba0e28fe8cbc49dc002708b335379669f17b597f
Jeferson <jsluiz@tecgraf.puc-rio.br>
2018-06-18 10:14:09 -0300

    adicionando pasta e 3 arquivos para concluir primeira missão estelar

---
+++ missao-estelar/jefersonluiz/js/js.js
@@ -0,0 +1,14 @@
+//declaração de variáveis
+var missao = "Missão Estelar JS em execução!!!";
+
+
+//declaração de funções:
+
+function eventoOnload() {
+    console.log("Evento on load disparado!");
+}
+
+function eventoOnclick(){
+    console.log("Missão Estelas JS realizada com super suecesso!!!");
+}
+
