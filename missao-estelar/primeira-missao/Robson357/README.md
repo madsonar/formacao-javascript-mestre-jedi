94e4dd7573497e0a3d8e76ffd31c45d80c112c03
robson357 <robsonti.system32@gmail.com>
2018-07-06 23:09:15 -0300

    Missão Estelar JS -  Realizada com sucesso

---
+++ missao-estelar/primeira-missao/Robson357/README.md
@@ -0,0 +1,18 @@
+ - JavaScript
+ - Java 
+ - HTML5
+ - PHP
+ - CSS
+ - HTML
+ - Bootstrap
+ - XML
+ - WebService
+ - React
+ - Ionic
+ - Laravel
+ - Nodjs
+ - Angular
+ - XHTML
+ - Kotlin
+ - OOP
+
---
+++ missao-estelar/primeira-missao/Robson357/js/js.js
@@ -0,0 +1,11 @@
+//declaração de Variáveis
+var missao = "Missão Estelar JS em execução !!";
+
+//Declaração de funções
+function eventoOnload(){
+    console.log("Evento onload disparado");
+}
+
+function eventoOnclick(){
+    console.log("Missão Estelar JS realizada com sucesso!!");
+}
