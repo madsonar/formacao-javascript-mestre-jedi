0d41933cf7ab2e5049d5abe1c070a20ebc59e1e8
VictorGeruso <victor.geruso@gmail.com>
2018-07-11 18:59:32 -0300

    Missão estelar realizada

---
+++ missao-estelar/primeira-missao/VictorGeruso/js/js.js
@@ -0,0 +1,9 @@
+var missão = "Missão Estelar JS em execução!!!";
+
+function eventoOnload() {
+    console.log("Evento onload disparado");
+}
+
+function eventoOnClick() {
+    console.log("Missão Estelar JS realizada com sucesso!!!");
+}
