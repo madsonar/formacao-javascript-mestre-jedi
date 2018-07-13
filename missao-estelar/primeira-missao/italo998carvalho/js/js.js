feb8fba6f42bf64cbe9b88a8af3a85902be2a032
Italo Carvalho <italo.998.carvalho@gmail.com>
2018-07-13 10:52:57 -0300

    Primeira missão estelar

---
+++ missao-estelar/primeira-missao/italo998carvalho/js/js.js
@@ -0,0 +1,9 @@
+function eventoOnLoad(){
+    console.log("Evento onload disparado");
+}
+
+var missao = "Missao Estelar JS em execução!!!";
+
+function eventoOnClick(){
+    console.log("Missão Estelar JS realizada com sucesso!!!");
+}
