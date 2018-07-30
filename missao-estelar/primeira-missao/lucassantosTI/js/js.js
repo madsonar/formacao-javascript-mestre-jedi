bd3701e61a2119a6784ffd2413001843251d63e2
Lucas Daniel dos Santos <lucassantos.ti@live.com>
2018-07-30 13:51:03 -0300

    Include my personal folder and add mission files to this directory

---
+++ missao-estelar/primeira-missao/lucassantosTI/js/js.js
@@ -0,0 +1,10 @@
+// Declaração de variaveis
+var missao = 'Missão Estelar JS em execução!!!';
+
+// Functions
+function eventoOnLoad(){
+    console.log('Evento onload disparado');
+}
+function eventoOnClick(){
+    console.log('Missão Estelar JS realizada com sucesso!!!');
+}
