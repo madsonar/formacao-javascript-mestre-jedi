936935e1c855aa6214882fc60a113f0435316ae0
PBconserta <claudio.c.lima@hotmail.com>
2018-08-26 17:20:57 -0300

    Atualização em 26/08/2018

--- variaveis-e-identificadores/js/js.js
+++ variaveis-e-identificadores/js/js.js
@@ -3,4 +3,4 @@ nome = "Pedro";
 var idade = 23;
 var pretensaoSalarial = 3277.22;
 var valeRefeicao = true;
-
+ console.log("Nome: " + nome + " Idade: " + idade + " Salário: " + pretensaoSalarial + " Vale Refeição: " + valeRefeicao);
