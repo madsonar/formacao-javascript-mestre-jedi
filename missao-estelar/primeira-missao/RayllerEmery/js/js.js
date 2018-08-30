3cfa18b313b831a5b058d03922bc01a31142a35e
Rayller <rayller.emery@gmail.com>
2018-08-30 00:14:48 -0300

    Enviado por RayllerEmery

---
+++ missao-estelar/primeira-missao/RayllerEmery/js/js.js
@@ -0,0 +1,10 @@
+function eventoOnload(){
+    console.log('Evento onload disparado');
+}
+
+var missao = 'Missão Estelar JS em Execução!!!';
+
+function eventoOnClick(){
+    console.log(missao);
+}
+
