388046937e3864ad097b3111c4573dab9a5bfc37
matheus-ndc <matheusndc@mail.com>
2018-09-03 17:47:50 -0400

    Primeira Missão

---
+++ missao-estelar/primeira-missao/matheus-ndc/js/js.js
@@ -0,0 +1,11 @@
+var missao = 'Missão Estelar JS em execução!!!';
+
+function eventoOnload()
+{
+    console.log('Evento onload disparado');
+}
+
+function eventoOnclick()
+{
+    console.log('Missão Estelar JS realizada com sucesso!!!');
+}
