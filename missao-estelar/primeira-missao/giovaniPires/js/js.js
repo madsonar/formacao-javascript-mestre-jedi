a29cfc6fdeae7e7034aab80b66d8f7c473d989aa
giovanipires <giovani.pires@gmil.com>
2018-09-10 14:50:21 -0300

    prmeira missao estelar giovanipires

---
+++ missao-estelar/primeira-missao/giovaniPires/js/js.js
@@ -0,0 +1,5 @@
+// 10 - Crie outra função chamada 'eventoOnclick' e através do método console.log informe o texto 'Missão Estelar JS realizada com sucesso!!!'
+
+function eventoOnclick(){
+    console.log("Missão Estelar JS realizada com sucesso!!!");
+}
