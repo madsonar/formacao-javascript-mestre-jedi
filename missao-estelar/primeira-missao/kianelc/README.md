03e2fe7cad0d76fd284a693d75b6ec3fb0a6ddca
Kiane L. Casagrande <kiane.casagrande@hotmail.com>
2018-09-30 22:55:56 -0300

    Primeira Missão Estelar JS realizada com sucesso!

---
+++ missao-estelar/primeira-missao/kianelc/README.md
@@ -0,0 +1,14 @@
+# Missão estelar nº 1
+
+> Criado o arquivo README.md
+> Informe suas tecnologias preferidas
+
+## Tecnologias preferidas
+
+> HTML;
+> CSS;
+> JavaScript;
+> JQuery;
+> Vue.js;
+> Vuetify;
+> Bootstrap;
---
+++ missao-estelar/primeira-missao/kianelc/js/js.js
@@ -0,0 +1,9 @@
+function eventoOnload() {
+    console.log("Evento onload disparado");
+}
+
+var missao = "Missão Estelar JS em execução!!!";
+
+function eventoOnclick(params) {
+    console.log("Missão Estelar JS realizada com sucesso!!!");
+}
