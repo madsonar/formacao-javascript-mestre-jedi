6099ee4f13839ba5cb1a3b139a2bab9d508992f7
Gabriel Marques Aragão <gabriel.aragao.comp@gmail.com>
2018-10-07 16:39:20 -0300

    Add primeira missao estelar Gabriel

---
+++ missao-estelar/primeira-missao/gabriel-m-aragao/js/js.js
@@ -0,0 +1,9 @@
+var missao="missao estelar JS em execucao!";
+
+function eventoOnLoad(){
+    console.log("Evento onload disparado");
+}
+
+function eventoOnClick(){
+    console.log("Missao estelar JS realizada com sucesso");
+}
