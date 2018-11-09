0688f4142891aa40f39f14f1dc5eeffaaf3fe6b1
kleitonalbuquerque <kleiton2102@gmail.com>
2018-11-09 01:51:32 -0200

    comentários  no código

--- missao-estelar/primeira-missao/kleitonalbuquerque/js/js.js
+++ missao-estelar/primeira-missao/kleitonalbuquerque/js/js.js
@@ -1,8 +1,10 @@
+// Declarando variáveis
+var missao = "Missão Estelar JS em execução!!!"
+
+// Declarando funções
 function eventoOnload() {
     console.log("Evento onload disparado")
 }
-
-var missao = "Missão Estelar JS em execução!!!"
 
 function eventoOnclick() {
     console.log("Missão Estelar JS realizada com sucesso!!!")
