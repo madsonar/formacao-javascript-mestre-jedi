c58729122c13f1bfcc847ccb102f00db22c66f0b
flaviusanderson <flavius.anderson@gmail.com>
2018-11-26 17:32:57 -0300

    Adicionando Primeira Missão de Flávius Anderson

---
+++ missao-estelar/primeira-missao/flaviusanderson/js/js.js
@@ -0,0 +1,17 @@
+// Variáveis
+var missao = "Missão Estelar JS em Execução !!!";
+
+// Declarações das Funções
+function eventoOnLoad(){
+    console.log("Evento onload disparado.");
+}
+
+function eventoOnClick(){
+    console.log("Missão Estelar JS realizada com sucesso!!!");
+}
+
+
+
+
+
+
