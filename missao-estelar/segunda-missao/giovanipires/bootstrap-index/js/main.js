882aa6b4b6cc7af08b33b642e7a11353b777ab29
Giovani Pires <giovani.pires@gmail.com>
2018-11-27 15:19:24 -0200

    Segunda missão estelar, atividades de 5 a 8 realizadas com sucesso.

---
+++ missao-estelar/segunda-missao/giovanipires/bootstrap-index/js/main.js
@@ -0,0 +1,13 @@
+//Tarefa 6
+//Segunda Missão Estelar JS
+
+//Tarefa 7
+/*
+Apertem os cintos para adentramos
+ao hiper-espaço rumo a uma nova
+Missão Estelar JS!:)
+*/
+
+//Tarefa 8
+var missao = "Segunda Missão Estelar JS";
+console.log("var missao é = " + missao + "do tipo " + typeof missao);
