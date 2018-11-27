d97d84ce38f7c912ef47e9f79b4e536dd3b8e09c
Zezinho <zeverardino@gmail.com>
2018-11-27 11:40:42 -0200

    primeira missao

---
+++ missao-estelar/primeira-missao/joseLuizVerardino/js/js.js
@@ -0,0 +1,10 @@
+function eventoOnload() {
+    console.log('Evento onload disparado');
+    var missao;
+    missao = 'Missão Estelar JS em execução!!!';
+    console.log(missao);
+}
+
+function eventoOnclick() {
+    console.log('Missão Estelar JS realizada com sucesso!!!');
+}
