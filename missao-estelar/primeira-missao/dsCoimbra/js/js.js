d58cb0a0b1c71384b0f1467a8fde362da54e1b81
dsCoimbra <ds.coimbra96@gmail.com>
2019-02-06 02:33:03 -0200

    Criado o index.html e js.js

---
+++ missao-estelar/primeira-missao/dsCoimbra/js/js.js
@@ -0,0 +1,10 @@
+var missao = "Missão Estelar Js em execução";
+
+function eventoOnLoad(){
+    console.log("Evento OnLoad disparado");
+}
+
+function eventoOnClick(){
+    console.log("Missão Estelar JS realizada com sucesso!");
+}
+
