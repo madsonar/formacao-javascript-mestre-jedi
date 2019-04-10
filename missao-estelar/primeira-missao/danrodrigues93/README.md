dc1528582d2bcba36ba1863fdb4d2ab89a06deaa
danrodrigues93 <danilo_russini@hotmail.com>
2019-04-10 11:54:39 -0300

    missão-estelar

---
+++ missao-estelar/primeira-missao/danrodrigues93/README.md
@@ -0,0 +1,8 @@
+#Danilo Russini Rodrigues
+##Tecnologias preferidas
+JavaScript
+Java
+C#
+C++
+PHP
+HTML + CSS
---
+++ missao-estelar/primeira-missao/danrodrigues93/js/js.js
@@ -0,0 +1,9 @@
+function eventoOnLoad(){
+    console.log("evento onload disparado");
+}
+
+var missao = "Evento Estelar JS em execução!"
+
+function eventoOnClick(){
+    console.log('Missão Estelar JS realizada com sucesso!!!');
+}
