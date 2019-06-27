e7fe4e7e77e0f63af858c653b4c90aeb6a184f89
azevedorgabriel <azevedorgabriel@gmail.com>
2019-06-27 16:11:45 -0300

    commit primeira missão estelar

---
+++ missao-estelar/primeira-missao/azevedorgabriel/README.md
@@ -0,0 +1,5 @@
+# Tecnologias
+
+- PHP
+- JS
+- BD
---
+++ missao-estelar/primeira-missao/azevedorgabriel/js/js.js
@@ -0,0 +1,9 @@
+function eventoOnload() {
+    console.log("Evendo OnLoad disparado!");
+}
+
+var missao = "Missão Estelar JS em execução!!!";
+
+function eventoOnclick() {
+    alert("Missão Estelar JS realizada com sucesso!!!");
+}
