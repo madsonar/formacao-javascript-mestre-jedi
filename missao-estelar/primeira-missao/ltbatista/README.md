1d3db530e08e340f10b2512556d3e06a4f56bc05
ltbatista <ltbatista30@gmail.com>
2019-07-04 10:31:46 -0300

    realizando o commit dos arquivos

---
+++ missao-estelar/primeira-missao/ltbatista/README.md
@@ -0,0 +1,4 @@
+Minhas tecnologias favoritas são:
+
+- Python, Hadoop, HIVE, IBM Planning Analytics, SQL, PLSQL, amo desenvolver para banco de dados. E o javascript agora está se tornando uma paixão junto com Git e Github.
+
---
+++ missao-estelar/primeira-missao/ltbatista/js/js.js
@@ -0,0 +1,9 @@
+function eventoOnLoad(){
+    console.log("Evento OnLoad disparado");
+};
+
+var missao = "Missao Estelar JS realizada com sucesso!";
+
+function eventoOnClick(){
+    console.log("Missao estelar js realizada com sucesso!")
+};
