dd5ce8c7a42c138492829e603dde4ea1ae40ed08
lePradal <le.pradal.lopes@gmail.com>
2019-08-03 14:50:49 -0300

    Commit lePradal

---
+++ missao-estelar/segunda-missao/lePradal/js/main.js
@@ -0,0 +1,10 @@
+//Segunda Missão Estelar JS
+
+/*
+
+    Apertem os cintos para adentramos ao hiper-espaço rumo a uma nova Missão Estelar JS!:)
+
+*/
+
+let missao = "Segunda Missão Estelar JS";
+console.log(missao);
