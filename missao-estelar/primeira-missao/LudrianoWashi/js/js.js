b2130f474b598d005a060c8a441f8ee34e077061
LudrianoWashi <ludinho.washi@gmail.com>
2019-08-05 23:08:36 -0300

    alteração do arquivo js.js e index.html para testar conhecimento

--- missao-estelar/primeira-missao/LudrianoWashi/js/js.js
+++ missao-estelar/primeira-missao/LudrianoWashi/js/js.js
@@ -1,1 +1,10 @@
-console.log('Meu teste no verdadeiro');
+
+function eventoOnload(){
+    console.log('Onload disparado');
+};
+
+var missao = 'Missão Estelar JS em execução';
+
+function eventoOnclick(){
+    console.log('Missão Estelar JS executada com sucesso!!')
+}
