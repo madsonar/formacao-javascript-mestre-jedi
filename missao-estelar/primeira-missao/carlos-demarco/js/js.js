11ce0ebbc213832d9b3075c8f7945cbc56db8cab
Carlos De Marco <car.demarco@gmail.com>
2019-08-07 15:14:41 -0300

    Primeira Missao

---
+++ missao-estelar/primeira-missao/carlos-demarco/js/js.js
@@ -0,0 +1,11 @@
+// Variavéis
+var missao = "Missão Estelar JS em execução!!!";
+
+// Funções
+function eventoOnload() {
+    console.log("Evento onload disparado");
+}
+
+function eventoOnclick() {
+    console.log("Missão Estelar JS realizada com sucesso!!!");
+}
