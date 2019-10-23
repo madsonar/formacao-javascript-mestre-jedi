6cbaece1fa7ba9478d9e16834b722e16944c87cd
edu94du <edu_jobsz@hotmail.com>
2019-10-23 13:29:19 -0300

    alterado comentario

--- missao-estelar/primeira-missao/edu94du/JS/js.js
+++ missao-estelar/primeira-missao/edu94du/JS/js.js
@@ -1,9 +1,11 @@
 
+
 var missao = "Missão Estrelar em execução";
 
 function onLoad() {
     console.log('Evento onload disparado');
 }
+//Função onclick para aparecer no console do navegador
 function onClick() {
     console.log('Missão Estrelar realizada com sucesso');
 }
