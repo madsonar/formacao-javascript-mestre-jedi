5d49270cd81c363baa4d12d9b7b8e2dba5a11aa0
amanini <alexandre.manini@gmail.com>
2019-11-08 16:59:57 -0300

    amanini-missão estelar js

--- PROJETOS/Projeto-Galeria-deImagens-WEB-RESTFul-CRUD-ANGULAR-HTML5-Bootstrap-JS-Node-MySQL/rest-api/banco/script-table.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE galeria (
-    id_galeria INTEGER UNSIGNED  NOT NULL   AUTO_INCREMENT,
-    titulo VARCHAR(255)  NULL  ,
-    caminho TEXT  NULL    ,
-  PRIMARY KEY(id_galeria));
--- operadores/js-operadores/condicional-ternario.js
+++
@@ -1,8 +0,0 @@
-var resultado = (20 == 40) ? "Retornou true - 1" : "Retornou false - 1";
-console.log(resultado);
-
-if(20 == 40){
-    console.log("Retornou true - 2");
-}else{
-    console.log("Retornou false - 2");
-}
