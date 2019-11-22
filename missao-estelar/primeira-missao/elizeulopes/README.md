4d4b2219e900307aa2927a2db2b1d2fd6d7fbe39
loopsartes <loopsartes@gmail.com>
2019-11-22 02:54:25 -0300

    Missão Estelar JS realizada com sucesso

---
+++ missao-estelar/primeira-missao/elizeulopes/README.md
@@ -0,0 +1,4 @@
+- Java
+- JavaScript
+- css
+- html
---
+++ missao-estelar/primeira-missao/elizeulopes/js/js.js
@@ -0,0 +1,12 @@
+//declaração de variável
+var missao = "Missão Estelar JS em execução!"
+
+//declaração de função
+function eventoOnload (){
+    console.log('Evento onload disparado')
+}
+
+
+function eventoOnclick(){
+    console.log('Missão Estela JS realizada com sucesso!')
+}
