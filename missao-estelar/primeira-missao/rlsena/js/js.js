540d930a7365f3f090b099f98c63dc763714e76f
rlsena <rlsena@gmail.com>
2019-11-28 19:47:21 -0300

    missao estelar JS - rlsena

---
+++ missao-estelar/primeira-missao/rlsena/js/js.js
@@ -0,0 +1,11 @@
+var missao="Missão Estelar JS em execução!!!"
+
+console.log(missao);
+
+function eventoOnload(){
+    console.log("Evento Onload disparado");
+}
+
+function eventoOnclick(){
+    console.log("Missão Estelar JS realizada com sucesso!!!");
+}
