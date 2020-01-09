b2d514368d1355a89a9b4cfe6e4e52a192ffff36
Nathan Gabriel <nathangabriel27@gmail.com>
2020-01-09 18:22:06 -0300

    Corrigindo algumas coisas

--- missao-estelar/primeira-missao/NathanGabriel27/js/index.js
+++ missao-estelar/primeira-missao/NathanGabriel27/js/index.js
@@ -1,9 +1,9 @@
-console.log("teste");
+
+
 function eventoOnload(){
   console.log("Evento onload disparado");
   
 }
-eventoOnload()
 var missao = "Missãso Estelar JS em execução"
 
 function eventoOnclick(){
