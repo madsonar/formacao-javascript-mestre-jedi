45a618535c376635ad72cd381872445a2587dcb4
Victor <victor.qcc@gmail.com>
2020-02-09 12:46:58 -0300

    primeira missao estelar

---
+++ missao-estelar/primeira-missao/VictorQueiroga/js/js.js
@@ -0,0 +1,8 @@
+function eventoOnLoad(){
+    console.log("Evento onload disparado");
+}
+var missao = "Missão estelar JS em execução";
+
+function eventoOnClick(){
+    console.log("Missão estelar JS realizada com sucesso");
+}
