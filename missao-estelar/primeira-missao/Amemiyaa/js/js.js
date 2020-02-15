f850a8e0b4ef408e769a237ad3381864d24e6456
Amemiya__ <leandro.amemiya@gmail.com>
2020-02-15 15:47:48 -0300

    Missão Estelar JS Realizada com sucesso!!! by Amemiya__

---
+++ missao-estelar/primeira-missao/Amemiyaa/js/js.js
@@ -0,0 +1,15 @@
+//declaração de variavéis
+var bemvindo= "Bem-vindo";
+var missao = "Primeira Missão Estelar";
+
+console.log(bemvindo + " a sua " + missao);
+
+//declaração de funções
+function eventoOnload(){
+    console.log ("Evento Onload disparado");
+}
+function eventoOnclick(){
+    alert("Você concluiu sua Primeira Missão Estelar");
+    console.log("Você terminou sua missão, Jovem Padawan.");
+}
+
