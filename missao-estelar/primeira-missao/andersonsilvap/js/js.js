d0360fe888639ad4b8fb44062ce553ee3d686c3a
andersonsilvap <andersonmistik@gmail.com>
2020-03-28 12:35:46 -0300

    add alterações js

--- missao-estelar/primeira-missao/andersonsilvap/js/js.js
+++ missao-estelar/primeira-missao/andersonsilvap/js/js.js
@@ -1,3 +1,9 @@
 function eventoOnload(){
-    console.log("Evento onload disparado")
+    console.log("Evento onload disparado.")
+}
+
+var missao = ("Missão Estelar JS em execução.")
+
+function eventoOnclick(){
+    console.log("Missão Estelar JS realizada com sucesso!!!")
 }
