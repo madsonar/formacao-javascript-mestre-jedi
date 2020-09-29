66bf555c8dbeb666f2ef3e943239ea5945aef9c9
Demetrius Batista dos Santos <dbsfox@outlook.com>
2020-09-29 14:14:05 -0300

    Alter

---
+++ missao-estelar/primeira-missao/DemetriusBS/js/js.js
@@ -0,0 +1,14 @@
+//Variável
+var missao = "Missão Estelar JS em execução!!!";
+
+//Funções
+function eventoOnload(){
+    console.log("Evento onload disparado!");
+}
+
+function eventoOnclick(){
+    console.log("Missão Estelar JS realizada com sucesso!!!");
+}
+
+
+
