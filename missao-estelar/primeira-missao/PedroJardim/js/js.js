5b59a5c33dc5be2fb85794b5efabe40ca1971753
PedroHJ <pedrohj1000@hotmail.com>
2021-01-06 16:45:18 -0300

    Primeiro Commit Pedro Jardim

---
+++ missao-estelar/primeira-missao/PedroJardim/js/js.js
@@ -0,0 +1,10 @@
+var missao = 'Missão Estelar JS em execução!!!'
+
+function eventoOnload() {
+  console.log('Evento Onload disparado');
+}
+
+function eventoOnclick() {
+  console.log('Missão Estelar JS realizada com sucesso!!!');
+}
+
