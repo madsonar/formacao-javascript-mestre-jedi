7af3b19010e24108ecaf4a9a34a82bd0c5688cc4
eddie-01 <edmon2010@hotmail.com>
2021-04-14 15:12:27 -0300

    Missão estelar 01

---
+++ missao-estelar/primeira-missao/eddie-01/js/js.js
@@ -0,0 +1,9 @@
+function eventoOnload(){
+    console.log ('Evento onload disparado')
+}
+
+var missão = 'MISSÃO ESTELAR JS em execução!!!'
+
+function eventoOnclick(){
+    console.log ('Missão Estelar JS realizada com sucesso!!!')
+}
