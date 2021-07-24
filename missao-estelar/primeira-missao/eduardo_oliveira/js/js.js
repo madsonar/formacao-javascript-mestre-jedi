2c29b7ecb7477a788ec9c4bfe65695feda1741df
EdOliveiraJ <edjdevel@gmail.com>
2021-07-24 14:33:20 -0300

    Missão Estelar JS Realizada com Sucesso!!! :)

--- missao-estelar/primeira-missao/eduardo_oliveira/js/js.js
+++
@@ -1,9 +0,0 @@
-function eventoOnLoad(){
-    console.log('Evento onload disparado')
-};
-
-var missao = 'Missão Estelar em execução!!!'
-
-function eventoOnClick(){
-    console.log('Missão Estelar JS realizada com sucesso!!!')
-};
