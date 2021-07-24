c05335ebcd9c4b5497f83a0d800b2a432c12aeec
EdOliveiraJ <edjdevel@gmail.com>
2021-07-24 13:49:57 -0300

    arquivos incluidos

---
+++ missao-estelar/primeira-missao/eduardo_oliveira/js/js.js
@@ -0,0 +1,9 @@
+function eventoOnLoad(){
+    console.log('Evento onload disparado')
+};
+
+var missao = 'Missão Estelar em execução!!!'
+
+function eventoOnClick(){
+    console.log('Missão Estelar JS realizada com sucesso!!!')
+};
---
+++ missao-estelar/primeira-missao/eduardo_oliveira/readme.md
@@ -0,0 +1,10 @@
+# Tecnologias Preferidas
+## JavaScript
+## HTML5
+## CSS3
+## NodeJs
+## PHP
+## Python
+## C#
+## Java
+
