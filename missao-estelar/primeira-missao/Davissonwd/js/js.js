aaa69e7ee25f75f2054e1e9bf5e9a4da1e81dea7
wanderson <wanderson.davisson@fdte.io>
2021-08-11 01:14:57 -0300

    Misssao realizada

---
+++ missao-estelar/primeira-missao/Davissonwd/js/js.js
@@ -0,0 +1,11 @@
+function eventoOnload(){
+    console.log("Evento disparado")
+}
+
+const missao = "Missão Estelar JS em execução!!!"
+
+
+function eventoOnclick (){
+    console.log("Missão Estelar JS realizada com sucesso!!!")
+}
+
