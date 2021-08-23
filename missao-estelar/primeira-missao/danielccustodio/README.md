4503be8a4c63221dc9097be4a676ed76ca76fa07
Daniel Custodio <danielccustodio@hotmail.com>
2021-08-23 16:45:10 -0300

    commit danielccustodio - primeira missao

---
+++ missao-estelar/primeira-missao/danielccustodio/README.md
@@ -0,0 +1,36 @@
+# Tecnologias Preferidas
+
+- Python
+    - Django
+    - Scrapy
+    - Selenium
+
+- HTML
+- CSS
+- SASS
+
+- ElasticSearch
+
+- Pascal
+- Delphi
+
+## Aprendendo ...
+
+- JavaScript
+    - NodeJs
+    - React
+    - Vue
+    - Angular
+    - Gatsby
+    - NextJS
+
+## Interesse ...
+
+- PHP
+- Ruby
+    - RubyOnRails
+    - Jekyll
+- BD
+    - SQL
+    - Oracle
+    - Postgrees
---
+++ missao-estelar/primeira-missao/danielccustodio/js/js.js
@@ -0,0 +1,14 @@
+// Pre-Req-01 - Dentro do arquivo js crie uma função chamada 'eventoOnload' e comando o método console.log informe o texto 'Evento onload disparado';
+// Pre-Req-02 - Ainda no arquivo js crie uma variável de nome 'missao' e sete o valor 'Missão Estelar JS em execução!!!'
+// Pre-Req-03 - Crie outra função chamada 'eventoOnclick' e através do método console.log informe o texto 'Missão Estelar JS realizada com sucesso!!!'
+
+var missao = "Missão Estelar JS em execução!!!"
+
+function eventoOnLoad(){
+    console.log("Evento onload disparado");
+}
+
+function eventoOnclick(){
+
+    console.log("Missão Estelar JS realizada com sucesso!!!");
+}
