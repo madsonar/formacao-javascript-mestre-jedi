e979c25c6a515e773950e937b1a799ef0c99ce76
biluadev <biluadevcod@gmail.com>
2021-08-29 10:37:43 +0100

    Missão Estelar JS Cumprida!

---
+++ missao-estelar/primeira-missao/biluadev/JS/js.js
@@ -0,0 +1,14 @@
+//DECLARAÇÃO DE VARIÁVEIS 
+var missao = "Missão Estelar JS em execução";
+
+//EVENTO
+function eventoOnload(){
+    //DISPARANDO O EVENTO
+console.log("eventoOnload disparado");
+}
+
+function eventoOnclick(){
+    //DISPARANDO O EVENTO
+console.log("Missão Estelar JS realizada com sucesso!!!");
+}
+
