0f4d4824b78ad119752084a5b7bffdaae0ee55ca
David Campos Machado <88288170+DCMachado@users.noreply.github.com>
2021-10-01 16:17:33 -0300

    adicionando pasta
    adicionando pasta de variaveis no projeto

---
+++ variaveis e identificadores/js/js.js
@@ -0,0 +1,17 @@
+
+
+//Declarações de variáveis
+var nome;
+nome = "Pedro";
+var idade = 23;
+var pretensaoSalarial = 3277.22;
+var valeRefeicao = true;
+
+/*
+xxxxxxxxx
+xxxxxxx
+xxxxxxxxxxx
+*/
+function calcTributo(){
+    sdekolf;
+}
