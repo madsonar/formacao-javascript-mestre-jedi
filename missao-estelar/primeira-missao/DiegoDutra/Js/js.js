db1d6a10bd3c25f37c5d12a328f5ecff1f360065
Diego Dutra <digo779@gmail.com>
2022-02-07 10:21:34 -0300

    missão estelar JS

---
+++ missao-estelar/primeira-missao/DiegoDutra/Js/js.js
@@ -0,0 +1,12 @@
+/* Variável */
+var missao = 'missão Estalar JS em execução';
+
+/* Função */
+function eventoOnload (){
+    console.log ('Evento onload disparado')
+}
+
+/* Função onclick */
+function eventoOnclick (){
+    console.log ('Missão Estelar JS realizada com sucesso!')
+}
