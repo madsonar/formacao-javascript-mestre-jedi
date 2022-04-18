308f0327f02f43cc4c2079af198cfa76c89a2a41
ThorVands <lupabuitrago@gmail.com>
2022-04-17 23:19:47 -0300

    Missão estelar completa com sucesso!!!

---
+++ missao-estelar/primeira-missao/ThorVands/README.md
@@ -0,0 +1,19 @@
+# Primeira Missão Estela!
+## Minhas Tecnologias favoritas:
+Html5 
+JavaScripst
+CSS
+Python
+Flutter
+Node
+Ruby
+C++
+VBA (autocad,excel)
+BlockChain
+Dart
+React Native
+Linux/Ubutun
+Android
+WSL2
+XML
+IFC
---
+++ missao-estelar/primeira-missao/ThorVands/js/js.js
@@ -0,0 +1,10 @@
+function eventoOnload(){
+    console.log("evento onload disparado !");
+}
+
+var missao = "Missão Estelar JS executada com Sucesso !!!";
+
+function eventoOnClick(){
+    console.log("Missão Estelar JS executada com Sucesso !!!");
+    alert(missao);
+}
