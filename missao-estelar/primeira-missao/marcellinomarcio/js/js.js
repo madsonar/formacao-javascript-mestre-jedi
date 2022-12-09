4a25afc6475252029e357300ad6955332523ef03
marcellinomarcio <marcellinomarcio@gmail.com>
2022-12-08 23:55:57 -0300

    Minha Primeira Missão

---
+++ missao-estelar/primeira-missao/marcellinomarcio/js/js.js
@@ -0,0 +1,11 @@
+"use strict";
+
+function eventoOnload(){
+    console.log("Evento onload disparado");
+}
+
+let missao = "Missão Estelar JS em execução!!!";
+
+function eventoOnclick(){
+    console.log("Missão Estelar JS realizada com sucesso!!!");
+}
