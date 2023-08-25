24114196882b9f1ce5ae2fd0337ad6f1c84a62a6
chamex23 <monteirokaka15@gmail.com>
2023-08-25 15:01:32 -0300

    Missão estelar JS - realizada com sucesso!! :)

---
+++ missao-estelar/primeira-missao/chamex23/js/js.js
@@ -0,0 +1,13 @@
+//declaração de variáveis
+
+var missao = "Missão estelar js em execução"
+
+
+//declaração de funções
+function eventOnload(){
+    console.log('Evento onLoad disparado')
+}
+
+function eventoOnClick(){
+    console.log('Missão estelar realizado com sucesso!')
+}
