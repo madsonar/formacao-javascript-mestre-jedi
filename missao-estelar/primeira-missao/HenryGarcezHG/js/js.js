6322b097ddf24258c8d947ee94d21f98eba08855
HenryGarcezHG <azrelatable@gmail.com>
2024-02-04 19:55:13 -0300

    Missão Estelar 01 realizada com sucesso

---
+++ missao-estelar/primeira-missao/HenryGarcezHG/js/js.js
@@ -0,0 +1,10 @@
+function eventoOnLoad() {
+    console.log("Evento onload disparado");
+};
+
+var missao = "Missão Estelar JS em Execução!!!";
+
+
+function eventoOnClick() {
+    console.log("Missão Estelar JS realizada com sucesso.")
+};
